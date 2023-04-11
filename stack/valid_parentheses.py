2e9173fe81041b77d68ddd9a568501525dc21d1b
xueliansniper <xueliansniper@gmail.com>
2023-04-11 11:48:42 +0800

    add codes

--- stack/valid_parentheses.py
+++ stack/valid_parentheses.py
@@ -1,4 +1,37 @@
 class ValidParentheses:
+    def __init__(self):
+        pass
+
+    def isValidV2(self, s: str) -> bool:
+        if not s or len(s) < 2:
+            return False
+        stack = []
+        for c in s:
+            if c == '(':
+                stack.append(')')
+            elif c == '[':
+                stack.append(']')
+            elif c == '{':
+                stack.append('}')
+            elif len(stack) == 0 or stack.pop() != c:
+                return False
+        return not stack
+
+    def isValidV1(self, s: str) -> bool:
+        if not s or len(s) < 2:
+            return False
+        stack = []
+        for c in s:
+            if c == '(':
+                stack.append(')')
+            elif c == '[':
+                stack.append(']')
+            elif c == '{':
+                stack.append('}')
+            elif not stack or stack.pop() != c:
+                return False
+        return not stack
+
     def isValid(self, s: str) -> bool:
         if not s or len(s) < 2:
             return False
@@ -17,3 +50,12 @@ class ValidParentheses:
                 if c == ']' and last != '[':
                     return False
         return not stack
+
+instance = ValidParentheses()
+#s = "(]"
+s = "()[]{}"
+res = instance.isValidV2(s)
+print(res)
+
+
+

1868645af75b1f85b006d94c9e4c323c5b4304d0
xueliansniper <xueliansniper@gmail.com>
2023-04-12 14:39:11 +0800

    add implementations of LC71

--- stack/simplify_path.py
+++ stack/simplify_path.py
@@ -5,6 +5,15 @@ class SimplifyPath:
     def __init__(self):
         pass
 
+    def simplifyPathV1(self, path: str) -> str:
+        stack = []
+        for x in path.split('/'):
+            if '..' == x and stack:
+                stack.pop()
+            elif x not in ['', '.', '..']:
+                stack.append(x)
+        return '/' + '/'.join(stack)
+
     def simplifyPath(self, path: str) -> str:
         stack = []
         arr = path.split('/')
